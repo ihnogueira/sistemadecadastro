cb10a0608a7c5ee16f58fad8ace3c2f41863ce24
ihnogueira <igor.henrique@outlook.com>
2023-08-11 10:18:30 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está s conectando ao banco de dados")
+// mais um console log de teste:
+console.log("O paulo fez ese commit sem você ver na aula!")
+console.log("enviando para o GitHub")
